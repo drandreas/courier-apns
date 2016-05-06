81e62c1526a2f8ae7e6723b0804aae9bfce44e05
AntiCat <contiki@anticat.ch>
2016-05-06 22:51:45 +0200

    First public release

--- README.md
+++ README.md
@@ -1,2 +1,112 @@
-# courier-apns
-A iOS Push Provider for Courier IMAP
+# iOS Push Provider for Courier IMAP
+Courier-APNs is a small daemon designed to add Push support to Courier IMAP. Even tough 
+the implementation is kept simplistic it was designed to be scalable. 
+
+## Features
+ - Uses APNS/2 and hence supports HTTP/2 connection
+ - Uses persistent connections to APNs
+ - Manages expired tokens
+ - Integration into Courier IMAP has a minimal risk
+
+## Caveats
+I have Courier-APNs running for several domains without any issues. However my systems
+handles a couple of hundred emails per day only. The code is not tested in high load situations.
+Moreover adding Push support to Courier IMAP requires changes to the source code.
+
+_Note:_ Courier-APNs assumes that all mail accounts share a single UID/GID i.e. mail:mail.
+
+## Prerequisites
+ - Google Go
+ - daemonize
+ - maildrop
+ - openbsd-netcat or socat
+
+## Install 
+- Obtain a valid Certificate for the Apple Push Notifications Service.
+  Stefan Arentz has a well written [guide](https://github.com/st3fan/dovecot-xaps-daemon).
+  If you don’t own an OS X Server you can try to sign up for a Certificate on 
+  [Apples Push Certificates Portal](https://identity.apple.com).
+- Install Courier-APNs
+```sh
+go get -u github.com/drandreas/courier-apns
+```
+- Launch Courier-APNs. It will display your UID. Keep it for later.
+```
+$GOPATH/bin/courier-apns /path/to/certificate /path/to/private/key
+2016/05/06 19:39:50 Loading certificate...
+2016/05/06 19:39:50 UID=com.apple.mail.XServer.647cc087-774a-41ab-b9e5-77fafef13b8e
+2016/05/06 19:39:50 Make sure your IMAP_XAPPLEPUSHSERVICE_TOPIC is set accordingly.
+2016/05/06 19:39:50 Establishing connection...
+2016/05/06 19:39:50 Waiting for request...
+```
+- Stop Courier-APNs `CTRL-C` and re-launch it as daemon:
+```sh
+daemonize -p /var/run/courier/courierapns.pid  \
+          -l /var/run/courier/courierapns.lock \
+          -u mail  \
+          $GOPATH/bin/courier-apns -d \
+                     /path/to/certificate \
+                     /path/to/private/key
+```
+- Patch Courier IMAP with `courier-0.75.0-imap.patch`, compile and install it.
+- Add `XAPPLEPUSHSERVICE` to `IMAP_CAPABILITY` in `/etc/courier/imapd`
+- Add `IMAP_XAPPLEPUSHSERVICE_TOPIC=YOUR_UID`
+- Resetart Courier IMAP and check if it is advertising `XAPPLEPUSHSERVICE`
+```sh
+telnet localhost 143
+Trying ::1...
+Connected to localhost.
+Escape character is '^]'.
+* OK [CAPABILITY ... XAPPLEPUSHSERVICE ...] Courier-IMAP ready.
+```
+- Add push to your mail delivery routine `/etc/courier/maildroprc`
+
+__netcat:__
+```
+# Push Notification...
+PUSH=`echo $HOME | nc.openbsd -U /var/run/courier/courierapns.socket`
+echo $PUSH
+```
+
+__socat:__
+```
+# Push Notification...
+PUSH=`echo /export/mailhome/andreas/ | socat /var/run/courier/courierapns.socket STDIN`
+#Note: echo $PUSH is not supported
+```
+_Note:_ You might need to re-add your mail account to your iDevice for it to recognize push support.
+
+## Debugging
+- Courier-APNs is verbose. If you run into issues check `/var/log/syslog` and `/var/log/mail.log` for hints.
+- Courier-APNs stores its data in `$Maildir/.push/`. For each device a separate file is created e.g.
+```
+{
+   "aps-account-id": "0715A26B-CA09-4730-A419-793000CA982E", 
+   "aps-device-token": "2918390218931890821908309283098109381029309829018310983092892829", 
+   "mailboxes": [ "INBOX", 
+                  "INBOX.Sent"
+                ]
+}
+```
+
+## Command line
+```
+courier-apns --help
+usage: courier-apns [<flags>] <crt> <key>
+
+Listens to on a Unix socket and performs mail notifications.
+
+This deamon supports persistent connections to APNs. The expected input on the Unix socket
+is a single line containing a path to a Maildir. The client can be as simple as a piped echo
+e.g. echo /path/to/maildir | nc.openbsd -U /var/run/courier/courierapns.socket.
+Flags:
+      --help     Show context-sensitive help (also try --help-long and --help-man).
+  -s, --socket="/var/run/courier/apsn.socket"  
+                 Path to use for Unix socket.
+  -d, --syslog   Use Syslog instead of STDERR.
+      --version  Show application version.
+
+Args:
+  <crt>  Path to certificate file.
+  <key>  Path to private key file.
+```
---
+++ courier-apns.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"log/syslog"
+	"net"
+	"os"
+	"path"
+	"time"
+
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+
+	"github.com/sideshow/apns2"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	optCrt  = kingpin.Arg("crt", "Path to certificate file.").Required().String()
+	optKey  = kingpin.Arg("key", "Path to private key file.").Required().String()
+	optSock = kingpin.Flag("socket", "Path to use for Unix socket.").
+		Default("/var/run/courier/courierapns.socket").Short('s').String()
+	optSyLog = kingpin.Flag("syslog", "Use Syslog instead of STDERR.").Short('d').Bool()
+	topic    string //Topic is extracted from the certificate during startup
+)
+
+// The Device data structure must match the JSON
+// structure stored by Courier-IMAPd.
+type Device struct {
+	ApsAccountId   string   `json:"aps-account-id"`
+	ApsDeviceToken string   `json:"aps-device-token"`
+	Mailboxes      []string `json:"mailboxes"`
+}
+
+// HandleRequest reads the path to a Maildir and performs
+// a push to all devices listed in Maildir/.push/.
+//
+// If pushing to a device fails with HTTP status code 410,
+// the coresponding subscription file is deleted.
+func HandleRequest(conn net.Conn, client *apns2.Client) {
+	defer conn.Close()
+
+	// read path
+	reader := bufio.NewReader(conn)
+	dir, _, err := reader.ReadLine()
+	if err != nil {
+		log.Print("Error: ", err)
+		return
+	}
+
+	// read devices
+	log.Printf("Push %s\n", dir)
+	files, err := ioutil.ReadDir(path.Join(string(dir), ".push"))
+	if err != nil || len(files) == 0 {
+		fmt.Fprintf(conn, "Push %s disabled\n", dir)
+		log.Printf("  disabled\n")
+		return
+	} else {
+		fmt.Fprintf(conn, "Push %s to", dir)
+	}
+	for _, file := range files {
+		data, err := ioutil.ReadFile(path.Join(string(dir), ".push", file.Name()))
+		if err != nil {
+			log.Print("Error:", err)
+			continue
+		}
+		device := Device{}
+		json.Unmarshal(data, &device)
+
+		// perform push
+		notification := &apns2.Notification{
+			Topic:       topic,
+			DeviceToken: device.ApsDeviceToken,
+			Expiration:  time.Now().Add(time.Hour),
+			Payload: fmt.Sprintf("{ \"aps\": { \"account-id\": \"%s\" }}",
+				device.ApsAccountId),
+		}
+		res, err := client.Push(notification)
+
+		// handle errors
+		if err != nil {
+			log.Fatal("  Error: ", err)
+		} else {
+			fmt.Fprintf(conn, " %s (%v)", device.ApsDeviceToken[0:8], res.StatusCode)
+			log.Printf("  Device: %s, Code: %v, Reason: %v\n", device.ApsDeviceToken[0:8],
+				res.StatusCode, res.Reason)
+			// Remove file if device token is no longer active for the topic
+			if res.StatusCode == 410 {
+				err = os.Remove(path.Join(string(dir), ".push", file.Name()))
+				if err != nil {
+					log.Print("Error:", err)
+				} else {
+					log.Printf("  Device: %s removed\n", device.ApsDeviceToken[0:8])
+				}
+			}
+		}
+	}
+	fmt.Fprintf(conn, "\n")
+}
+
+// ParsCertificate extracts OID 0.9.2342.19200300.100.1.1
+// from a certificate. It must be identical to the one
+// defined for Courier-IMAPd.
+//
+// Author: Stefan Arentz, License: MIT
+// Source: https://github.com/st3fan/dovecot-xaps-daemon/
+func ParsCertificate(bytes []byte) (string, error) {
+	block, _ := pem.Decode(bytes)
+	if block == nil {
+		return "", errors.New("Could not decode PEM block from certificate")
+	}
+
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return "", err
+	}
+
+	if len(cert.Subject.Names) == 0 {
+		return "", errors.New("Subject.Names is empty")
+	}
+
+	oidUid := []int{0, 9, 2342, 19200300, 100, 1, 1}
+	if !cert.Subject.Names[0].Type.Equal(oidUid) {
+		return "", errors.New("Did not find a Subject.Names[0] with type 0.9.2342.19200300.100.1.1")
+	}
+
+	return cert.Subject.Names[0].Value.(string), nil
+}
+
+func main() {
+	kingpin.UsageTemplate(kingpin.CompactUsageTemplate).Version("0.1").Author("Andreas Dröscher")
+	kingpin.CommandLine.Help = `Listens to on a Unix socket and performs mail notifications.
+	This deamon supports persistent connections to APNs. The expected input on the Unix socket
+	is a single line containing a path to a Maildir. The client can be as simple as a piped echo
+	e.g. echo /path/to/maildir | nc.openbsd -U /var/run/courier/courierapns.socket.`
+	kingpin.Parse()
+
+	// attach Logger to Syslog
+	if *optSyLog {
+		logwriter, err := syslog.New(syslog.LOG_NOTICE, "courierapns")
+		if err != nil {
+			log.Fatal("Error:", err)
+		}
+		log.SetOutput(logwriter)
+		log.SetFlags(0)
+	}
+
+	// load certificate and key
+	log.Printf("Loading certificate...\n")
+	crt, err := ioutil.ReadFile(*optCrt)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	key, err := ioutil.ReadFile(*optKey)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	// extract and output uid (oid 0.9.2342.19200300.100.1.1 see
+	//                         http://tools.ietf.org/html/rfc4519#section-2.39)
+	topic, err = ParsCertificate(crt)
+	log.Printf("UID=%s\n", topic)
+	log.Printf("Make sure your IMAP_XAPPLEPUSHSERVICE_TOPIC is set accordingly.\n")
+
+	// establish APNs connection
+	log.Printf("Establishing connection...\n")
+	cred, err := tls.X509KeyPair(crt, key)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	client := apns2.NewClient(cred)
+	client.Production()
+
+	// open socket and serve clients
+	log.Printf("Waiting for request...\n")
+	os.Remove(*optSock)
+	socket, err := net.Listen("unix", *optSock)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	err = os.Chmod(*optSock, 0777)
+	if err != nil {
+		log.Print("Warning:", err)
+	}
+	for {
+		conn, err := socket.Accept()
+		if err != nil {
+			log.Fatal("Error:", err)
+		} else {
+			HandleRequest(conn, client)
+		}
+	}
+}
